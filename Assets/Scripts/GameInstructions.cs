0f0a6bf859c92a939867e02dae182af35b50a97a
Samantha1205 <99206967+Samantha1205@users.noreply.github.com>
2023-01-19 18:20:30 -0500

    progress3

---
+++ Assets/Scripts/GameInstructions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    public string gameInstructions = "Move the player by using the WASD keys";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameInstructions);
+    }
+}
